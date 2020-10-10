e4610783c612ffa05b3f0955436335ba1292a5b3
Lautaro Pinilla <lpinilla@itba.edu.ar>
2020-10-10 17:31:07 -0300

    Corrección a min_distance.py

--- TP/resources/mision_a_marte/min_distance.py
+++ TP/resources/mision_a_marte/min_distance.py
@@ -4,8 +4,8 @@ data = []
 with open('distances', 'r') as f:
     for line in f:
         d = line[:-1].split('    ')
-        times.append(d[0])
-        data.append(d[1])
+        times.append(float(d[0]))
+        data.append(float(d[1]))
 
 min_val = data[0]
 min_idx = 0
